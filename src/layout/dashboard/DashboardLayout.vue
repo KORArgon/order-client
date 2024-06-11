6b82a98943683d76ae238920c2e436a982d8483d
LEE-HYO-SANG\hanshin <uon0707@naver.com>
2024-06-11 16:14:53 +0900

    git init

---
+++ src/layout/dashboard/DashboardLayout.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="wrapper">
+    <side-bar>
+      <template slot="links">
+        <sidebar-link
+          to="/popular"
+          :name="$t('sidebar.popular')"
+          icon="tim-icons icon-heart-2"
+        />
+
+        <sidebar-link
+          to="/main"
+          :name="$t('sidebar.main')"
+          icon="tim-icons icon-align-left-2"
+        />
+        <sidebar-link
+          to="/side"
+          :name="$t('sidebar.side')"
+          icon="tim-icons icon-align-left-2"
+        />
+        <sidebar-link
+          to="/drink"
+          :name="$t('sidebar.drink')"
+          icon="tim-icons icon-align-left-2"
+        />
+<!--    아래것 지움    -->
+<!--        <sidebar-link
+          to="/dashboard"
+          :name="$t('sidebar.dashboard')"
+          icon="tim-icons icon-chart-pie-36"
+        />
+        <sidebar-link
+          to="/icons"
+          :name="$t('sidebar.icons')"
+          icon="tim-icons icon-atom"
+        />
+        <sidebar-link
+          to="/maps"
+          :name="$t('sidebar.maps')"
+          icon="tim-icons icon-pin"
+        />
+        <sidebar-link
+          to="/notifications"
+          :name="$t('sidebar.notifications')"
+          icon="tim-icons icon-bell-55"
+        />
+        <sidebar-link
+          to="/profile"
+          :name="$t('sidebar.userProfile')"
+          icon="tim-icons icon-single-02"
+        />
+        <sidebar-link
+          to="/table-list"
+          :name="$t('sidebar.tableList')"
+          icon="tim-icons icon-puzzle-10"
+        />
+        <sidebar-link
+          to="/typography"
+          :name="$t('sidebar.typography')"
+          icon="tim-icons icon-align-center"
+        />
+        <sidebar-link
+          to="/dashboard?enableRTL=true"
+          :name="$t('sidebar.rtlSupport')"
+          icon="tim-icons icon-world"
+        />-->
+      </template>
+    </side-bar>
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content @click.native="toggleSidebar"> </dashboard-content>
+
+      <content-footer></content-footer>
+    </div>
+  </div>
+</template>
+<style lang="scss"></style>
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import ContentFooter from "./ContentFooter.vue";
+import DashboardContent from "./Content.vue";
+import MobileMenu from "./MobileMenu";
+export default {
+  components: {
+    TopNavbar,
+    ContentFooter,
+    DashboardContent,
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
+    },
+  },
+};
+</script>
---
+++ src/pages/Popular.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <card>
+        <div class="places-buttons">
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto text-center">
+              <h4 class="card-title">
+                <h4 slot="header">인기메뉴</h4>
+                <p class="category">Click to view notifications</p>
+              </h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-8 ml-auto mr-auto">
+              <div class="row">
+                <div class="col-md-4">
+                  <base-button
+                    type="primary"
+                    block
+                    @click="notifyVue('top', 'left')"
+                  >Top Left</base-button
+                  >
+                </div>
+                <div class="col-md-4">
+                  <base-button
+                    type="primary"
+                    block
+                    @click="notifyVue('top', 'center')"
+                  >Top Center</base-button
+                  >
+                </div>
+                <div class="col-md-4">
+                  <base-button
+                    type="primary"
+                    block
+                    @click="notifyVue('top', 'right')"
+                  >Top Right</base-button
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-8 ml-auto mr-auto">
+              <div class="row">
+                <div class="col-md-4">
+                  <base-button
+                    type="primary"
+                    block
+                    @click="notifyVue('bottom', 'left')"
+                  >Bottom Left</base-button
+                  >
+                </div>
+                <div class="col-md-4">
+                  <base-button
+                    type="primary"
+                    block
+                    @click="notifyVue('bottom', 'center')"
+                  >Bottom Center</base-button
+                  >
+                </div>
+                <div class="col-md-4">
+                  <base-button
+                    type="primary"
+                    block
+                    @click="notifyVue('bottom', 'right')"
+                  >Bottom Right</base-button
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </card>
+    </div>
+  </div>
+</template>
+<script>
+import NotificationTemplate from "./Notifications/NotificationTemplate";
+import { BaseAlert } from "@/components";
+
+export default {
+  components: {
+    BaseAlert,
+  },
+  data() {
+    return {
+      type: ["", "info", "success", "warning", "danger"],
+      notifications: {
+        topCenter: false,
+      },
+    };
+  },
+  methods: {
+    notifyVue(verticalAlign, horizontalAlign) {
+      const color = Math.floor(Math.random() * 4 + 1);
+      this.$notify({
+        component: NotificationTemplate,
+        icon: "tim-icons icon-bell-55",
+        horizontalAlign: horizontalAlign,
+        verticalAlign: verticalAlign,
+        type: this.type[color],
+        timeout: 0,
+      });
+    },
+  },
+};
+</script>
+<style></style>
